20bf751582d059c5e1297b0b63e6dd567dd5ac55
Richard Pascarelli <rpavesi84@gmail.com>
2019-09-25 19:20:21 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,15 +4,15 @@ require_relative "./user.rb"
 
 
 class Student < User
-  @@knowledge = []
+  
   
  def initialize 
-   @@knowledge
+   @knowledge = []
  end
 
   def learn(knowledge) 
    @knowledge << knowledge 
-  end
+
 
 
 end
